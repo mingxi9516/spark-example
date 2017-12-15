9e25638da0fd1cc2911db4bda11f55418bb04245
wanshan.qiu <wanshan.qiu@lantaiyuan.com>
2017-12-15 18:14:32 +0800

    11

--- examples/src/main/python/test/hiveread.py
+++ examples/src/main/python/test/hiveread.py
@@ -17,39 +17,25 @@
 
 from __future__ import print_function
 
-# $example on:spark_hive$
-from os.path import expanduser, join, abspath
-
 from pyspark.sql import SparkSession
-from pyspark.sql import Row
-
-# $example off:spark_hive$
-
 """
 A simple example demonstrating Spark SQL Hive integration.
-Run with:
-  ./bin/spark-submit examples/src/main/python/sql/hive.py
 """
 
 if __name__ == "__main__":
-    # $example on:spark_hive$
-    # warehouse_location points to the default location for managed databases and tables
-    warehouse_location = abspath('spark-warehouse')
 
     spark = SparkSession \
         .builder \
-        .config("spark.sql.warehouse.dir", warehouse_location) \
+        .master("local[*]") \
         .enableHiveSupport() \
         .getOrCreate()
-
-    # spark is an existing SparkSession
-    # spark.sql("SELECT trim(userId) userId,"
-    #           "trim(phoneModel) phoneModel,"
-    #           "trim(cityCode) cityCode,"
-    #           "cast(trim(longitude) as double) longitude,"
-    #           "cast(trim(latitude) as double) latitude,"
-    #           "timestamp(trim(currentTime)) currentTime,"
-    #           "trim(geocode6) region"
-    #           "FROM dc.dwd_user_gps_tmp").show
-    spark.sql("select * from  dc.dwd_user_gps_tmp").show
+    spark.sql("SELECT trim(userId) userId,"
+              "trim(phoneModel) phoneModel,"
+              "trim(cityCode) cityCode,"
+              "cast(trim(longitude) as double) longitude,"
+              "cast(trim(latitude) as double) latitude,"
+              "timestamp(trim(currentTime)) currentTime,"
+              "trim(geocode6) region "
+              "FROM dc.dwd_user_gps_tmp "
+              "WHERE phoneModel=865091024998281")
     spark.stop()
