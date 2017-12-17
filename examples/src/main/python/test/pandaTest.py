637b892b0ec6d9985a326c9ea202552d97fb1e9e
wash <858832387@qq.com>
2017-12-17 13:38:30 +0800

    11

--- examples/src/main/python/test/pandaTest.py
+++ examples/src/main/python/test/pandaTest.py
@@ -1,7 +1,25 @@
-from pandas import Series,DataFrame
-from numpy.random import randn
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-df = DataFrame(randn(10,5),columns=['A','B','C','D','E'],index = np.arange(0,100,10))
-df.plot()
+
+s = pd.Series([1, 3, 5, np.nan, 6, 8])
+dates = pd.date_range('20130101', periods=6)
+df =pd.DataFrame(np.random.randn(6, 4), index=dates, columns=list('ABCD'))
+df2 = pd.DataFrame({'A': 1.,
+                    'B': pd.Timestamp('20130102'),
+                    'C': pd.Series(1, index=list(range(4)), dtype='float32'),
+                    'D': np.array([3] * 4, dtype='int32'),
+                    'E': pd.Categorical(["test", "train", "test", "train"]),
+                    'F': 'foo'})
+ts = pd.Series(np.random.randn(1000), index=pd.date_range('1/1/2000', periods=1000))
+ts = ts.cumsum()
+ts.plot()
+
+df = pd.DataFrame(np.random.randn(1000, 4), index=ts.index,
+                              columns=['A', 'B', 'C', 'D'])
+df.plot();
+df3 = pd.DataFrame(np.random.randn(1000, 2), columns=['B', 'C']).cumsum()
+df3['A'] = pd.Series(list(range(len(df))))
+df3.plot(x='A', y='B')
 plt.show()
+s1 = pd.Series([1,2,3,4,5,6], index=pd.date_range('20130102', periods=6))
--- examples/src/main/python/test/userDisplay.py
+++ examples/src/main/python/test/userDisplay.py
@@ -7,20 +7,20 @@
 from pyspark.sql import SparkSession
 import matplotlib as mpl
 import matplotlib.pyplot as plt
-
 if __name__ == "__main__":
     mpl.rcParams['legend.fontsize'] = 10
-    fig = plt.figure()
     spark = SparkSession \
         .builder \
         .master("local[*]") \
-        .enableHiveSupport() \
         .getOrCreate()
-    df = spark.sql("""SELECT trim(phoneModel) phoneModel,
-                   cast(trim(longitude) as double) longitude,
-                   cast(trim(latitude) as double) latitude,
-                   timestamp(trim(currentTime)) currentTime 
-                   FROM dc.dwd_user_gps_tmp 
-                   WHERE phoneModel=865091024998281 AND cityCode=0755""").toPandas
+    df = spark.read.json("people.json").toPandas()
+    # Displays the content of the DataFrame to stdout
+    df.plot()
+    # df = spark.sql("""SELECT trim(phoneModel) phoneModel,
+    #                cast(trim(longitude) as double) longitude,
+    #                cast(trim(latitude) as double) latitude,
+    #                timestamp(trim(currentTime)) currentTime
+    #                FROM dc.dwd_user_gps_tmp
+    #                WHERE phoneModel=865091024998281 AND cityCode=0755""").toPandas
     plt.show()
     spark.stop()
